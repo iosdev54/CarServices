689288c7b872c0ac966e0f38dcb68f1daa5e14b1
iosdev54 <iosdev54@gmail.com>
2022-11-29 14:44:01 +0200

    Add a annotationView to the MapViewController.

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController {
-
+    
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
     
     @IBOutlet weak var mapView: MKMapView!
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupPlaceMark()
+        
+        mapView.delegate = self
     }
     
     @IBAction func closeVC() {
@@ -27,7 +30,7 @@ class MapViewController: UIViewController {
     private func setupPlaceMark() {
         
         guard let location = place.location else { return }
-         
+        
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(location) { [weak self] placemarks, error in
             guard let `self` = self else { return }
@@ -39,7 +42,7 @@ class MapViewController: UIViewController {
             guard let placemarks = placemarks else { return }
             
             let placemark = placemarks.first
-             
+            
             let annotation = MKPointAnnotation()
             annotation.title = self.place.name
             annotation.subtitle = self.place.type
@@ -51,7 +54,30 @@ class MapViewController: UIViewController {
             //Выделяем, слегка увеличиваем аннотацию
             self.mapView.selectAnnotation(annotation, animated: true)
         }
-        
     }
+    
+}
 
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        return annotationView
+    }
 }
