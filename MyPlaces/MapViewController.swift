5559f833e69ff0b3eeee05ad73e8ed6840d47084
iosdev54 <iosdev54@gmail.com>
2022-11-30 12:32:59 +0200

    Add user location.

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionInMetters = 10_000.00
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -21,6 +24,17 @@ class MapViewController: UIViewController {
         setupPlaceMark()
         
         mapView.delegate = self
+        
+        checkLocationServises()
+    }
+    
+    @IBAction func centerViewInUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: regionInMetters, longitudinalMeters: regionInMetters)
+            
+            mapView.setRegion(region, animated: true )
+        }
     }
     
     @IBAction func closeVC() {
@@ -56,6 +70,53 @@ class MapViewController: UIViewController {
         }
     }
     
+    private func checkLocationServises() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            //            checkLocationAuthorization()
+        } else {
+            //Алерт отображается если геолокация отключена глобально для всего устройства
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Location Services are Disabled", message: "To enable it go: Settings -> Privacy -> Location Services and turn On.")
+            }
+        }
+    }
+    
+    private func setupLocationManager() {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    /*  //Deprecated
+     private func checkLocationAuthorization() {
+     switch CLLocationManager.authorizationStatus() {
+     case .authorizedWhenInUse:
+     mapView.showsUserLocation = true
+     break
+     case .denied:
+     //Show alert controller
+     break
+     case .notDetermined:
+     locationManager.requestWhenInUseAuthorization()
+     case .restricted:
+     //Show alert controller
+     break
+     case .authorizedAlways:
+     break
+     @unknown default:
+     print("New case is available")
+     }
+     }
+     */
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -81,3 +142,29 @@ extension MapViewController: MKMapViewDelegate {
         return annotationView
     }
 }
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        //        checkLocationAuthorization()
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            showAlert(title: "Your Location is not Available", message: "To give permission Go to: Settings -> MyPlaces -> Location")
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            showAlert(title: "Your Location is not Available", message: "To give permission Go to: Settings -> MyPlaces -> Location")
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("New case is available")
+        }
+    }
+}
+
